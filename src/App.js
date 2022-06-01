1ad0d770d852ff7ce44a5b019879cec95c56b344
marianonu14 <marianonunezma@gmail.com>
2022-06-01 19:08:33 -0300

    useState functions ready

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import './styles/App.css';
+import { useState } from 'react';
 
-const count = 0
 function App() {
+  const [count, setCount] = useState(0);
+
+  const add = () => {
+    setCount(count + 1);
+  }
+
+  const reset = () => {
+    setCount(0);
+  }
+
   return (
     <div className="App">
       <header className="header-container">
-        <h1>Countainer App</h1>
+        <h1>Counting App</h1>
       </header>
       <div className="main-container">
         <div className="counter">{count}</div>
-        <button className='btn'>Add +</button>
-        <button className='btn'>Reset</button>
+        <button className='btn' onClick={add}>Add +</button>
+        <button className='btn' onClick={reset}>Reset</button>
       </div>
     </div>
   );
